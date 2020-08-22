c5d9d0453baf0796c97e81e727bf0a174814d3c5
Oscar Cordero <ocordero@applaudostudios.com>
2020-08-22 13:40:38 -0600

    first commit

---
+++ src/components/Charts/Candle.js
@@ -0,0 +1,190 @@
+import React from 'react';
+import { format } from 'd3-format';
+import { timeFormat } from 'd3-time-format';
+import { ChartCanvas, Chart } from 'react-stockcharts';
+import {
+  CandlestickSeries,
+  RSISeries,
+  LineSeries,
+} from 'react-stockcharts/lib/series';
+import { XAxis, YAxis } from 'react-stockcharts/lib/axes';
+import {
+  CrossHairCursor,
+  MouseCoordinateY,
+  MouseCoordinateX,
+} from 'react-stockcharts/lib/coordinates';
+import { ema, rsi, atr, sma } from 'react-stockcharts/lib/indicator';
+import { discontinuousTimeScaleProvider } from 'react-stockcharts/lib/scale';
+import {
+  RSITooltip,
+  OHLCTooltip,
+  SingleValueTooltip,
+} from 'react-stockcharts/lib/tooltip';
+import { last } from 'react-stockcharts/lib/utils';
+
+const CandleChart = ({ data: initialData }) => {
+  const mouseEdgeAppearance = {
+    textFill: '#542605',
+    stroke: '#05233B',
+    strokeOpacity: 1,
+    strokeWidth: 3,
+    arrowWidth: 5,
+    fill: '#BCDEFA',
+  };
+  console.log(initialData);
+
+  const ema26 = ema()
+    .id(0)
+    .options({ windowSize: 26 })
+    .merge((d, c) => {
+      d.ema26 = c;
+    })
+    .accessor(d => d.ema26);
+
+  const ema12 = ema()
+    .id(1)
+    .options({ windowSize: 12 })
+    .merge((d, c) => {
+      d.ema12 = c;
+    })
+    .accessor(d => d.ema12);
+
+  const smaVolume50 = sma()
+    .id(3)
+    .options({ windowSize: 50, sourcePath: 'volume' })
+    .merge((d, c) => {
+      d.smaVolume50 = c;
+    })
+    .accessor(d => d.smaVolume50);
+
+  const rsiCalculator = rsi()
+    .options({ windowSize: 14 })
+    .merge((d, c) => {
+      d.rsi = c;
+    })
+    .accessor(d => d.rsi);
+
+  const atr14 = atr()
+    .options({ windowSize: 14 })
+    .merge((d, c) => {
+      d.atr14 = c;
+    })
+    .accessor(d => d.atr14);
+
+  const calculatedData = ema26(
+    ema12(smaVolume50(rsiCalculator(atr14(initialData))))
+  );
+
+  const xScaleProvider = discontinuousTimeScaleProvider.inputDateAccessor(
+    d => new Date(d.date)
+  );
+
+  const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(
+    calculatedData
+  );
+
+  const start = xAccessor(last(data));
+  const end = xAccessor(data[Math.max(0, data.length - 150)]);
+  const xExtents = [start, end];
+  return (
+    <ChartCanvas
+      height={600}
+      width={1000}
+      ratio={1}
+      margin={{ left: 70, right: 70, top: 20, bottom: 30 }}
+      type="svg"
+      seriesName="MSFT"
+      data={data}
+      xScale={xScale}
+      xAccessor={xAccessor}
+      displayXAccessor={displayXAccessor}
+      xExtents={xExtents}
+    >
+      <Chart
+        id={1}
+        height={300}
+        yExtents={[d => [d.high, d.low], ema26.accessor(), ema12.accessor()]}
+        padding={{ top: 10, bottom: 20 }}
+      >
+        <XAxis
+          axisAt="bottom"
+          orient="bottom"
+          showTicks={false}
+          outerTickSize={0}
+        />
+        <YAxis axisAt="right" orient="right" ticks={5} />
+
+        <MouseCoordinateY
+          at="right"
+          orient="right"
+          displayFormat={format('.2f')}
+          {...mouseEdgeAppearance}
+        />
+
+        <OHLCTooltip origin={[-40, 0]} />
+        <CandlestickSeries />
+      </Chart>
+      <Chart
+        id={3}
+        yExtents={[0, 100]}
+        height={125}
+        origin={(w, h) => [0, h - 250]}
+      >
+        <XAxis
+          axisAt="bottom"
+          orient="bottom"
+          showTicks={false}
+          outerTickSize={0}
+        />
+        <YAxis axisAt="right" orient="right" tickValues={[30, 50, 70]} />
+        <MouseCoordinateY
+          at="right"
+          orient="right"
+          displayFormat={format('.2f')}
+        />
+
+        <RSISeries yAccessor={d => d.rsi} />
+
+        <RSITooltip
+          origin={[-38, 15]}
+          yAccessor={d => d.rsi}
+          options={rsiCalculator.options()}
+        />
+      </Chart>
+      <Chart
+        id={8}
+        yExtents={atr14.accessor()}
+        height={125}
+        origin={(w, h) => [0, h - 125]}
+        padding={{ top: 10, bottom: 10 }}
+      >
+        <XAxis axisAt="bottom" orient="bottom" />
+        <YAxis axisAt="right" orient="right" ticks={2} />
+
+        <MouseCoordinateX
+          at="bottom"
+          orient="bottom"
+          displayFormat={timeFormat('%Y-%m-%d')}
+        />
+        <MouseCoordinateY
+          at="right"
+          orient="right"
+          displayFormat={format('.2f')}
+        />
+
+        <LineSeries yAccessor={atr14.accessor()} stroke={atr14.stroke()} />
+        <SingleValueTooltip
+          yAccessor={atr14.accessor()}
+          yLabel={`ATR (${atr14.options().windowSize})`}
+          yDisplayFormat={format('.2f')}
+          /* valueStroke={atr14.stroke()} - optional prop */
+          /* labelStroke="#4682B4" - optional prop */
+          origin={[-40, 15]}
+        />
+      </Chart>
+      <CrossHairCursor />
+    </ChartCanvas>
+  );
+};
+
+export default CandleChart;
--- src/views/reports/DashboardView/Sales.js
+++ src/views/reports/DashboardView/Sales.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { Bar } from 'react-chartjs-2';
 import {
   Box,
   Button,
@@ -9,127 +8,80 @@ import {
   CardContent,
   CardHeader,
   Divider,
-  useTheme,
   makeStyles,
-  colors
+  Select,
+  MenuItem,
+  InputLabel,
 } from '@material-ui/core';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import CandleChart from 'src/components/Charts/Candle';
 
 const useStyles = makeStyles(() => ({
-  root: {}
+  root: {},
 }));
 
 const Sales = ({ className, ...rest }) => {
   const classes = useStyles();
-  const theme = useTheme();
+  const [crudeData, setCrudeData] = useState([]);
+  const [filter, setFilter] = useState('lastMonth');
 
-  const data = {
-    datasets: [
-      {
-        backgroundColor: colors.indigo[500],
-        data: [18, 5, 19, 27, 29, 19, 20],
-        label: 'This year'
-      },
-      {
-        backgroundColor: colors.grey[200],
-        data: [11, 20, 12, 29, 30, 25, 13],
-        label: 'Last year'
-      }
-    ],
-    labels: ['1 Aug', '2 Aug', '3 Aug', '4 Aug', '5 Aug', '6 Aug']
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`../data/crude/${filter}.json`);
+      const data = await response.json();
+      setCrudeData(data);
+    };
+    fetchData();
+  }, [filter]);
+
+  const preventDefault = e => {
+    e = e || window.event;
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.returnValue = false;
   };
 
-  const options = {
-    animation: false,
-    cornerRadius: 20,
-    layout: { padding: 0 },
-    legend: { display: false },
-    maintainAspectRatio: false,
-    responsive: true,
-    scales: {
-      xAxes: [
-        {
-          barThickness: 12,
-          maxBarThickness: 10,
-          barPercentage: 0.5,
-          categoryPercentage: 0.5,
-          ticks: {
-            fontColor: theme.palette.text.secondary
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false
-          }
-        }
-      ],
-      yAxes: [
-        {
-          ticks: {
-            fontColor: theme.palette.text.secondary,
-            beginAtZero: true,
-            min: 0
-          },
-          gridLines: {
-            borderDash: [2],
-            borderDashOffset: [2],
-            color: theme.palette.divider,
-            drawBorder: false,
-            zeroLineBorderDash: [2],
-            zeroLineBorderDashOffset: [2],
-            zeroLineColor: theme.palette.divider
-          }
-        }
-      ]
-    },
-    tooltips: {
-      backgroundColor: theme.palette.background.default,
-      bodyFontColor: theme.palette.text.secondary,
-      borderColor: theme.palette.divider,
-      borderWidth: 1,
-      enabled: true,
-      footerFontColor: theme.palette.text.secondary,
-      intersect: false,
-      mode: 'index',
-      titleFontColor: theme.palette.text.primary
-    }
+  const disableScroll = () => {
+    document.addEventListener('wheel', preventDefault, {
+      passive: false,
+    });
+  };
+
+  const enableScroll = () => {
+    document.removeEventListener('wheel', preventDefault, false);
   };
 
   return (
-    <Card
-      className={clsx(classes.root, className)}
-      {...rest}
-    >
+    <Card className={clsx(classes.root, className)} {...rest}>
       <CardHeader
-        action={(
-          <Button
-            endIcon={<ArrowDropDownIcon />}
-            size="small"
-            variant="text"
-          >
-            Last 7 days
-          </Button>
-        )}
+        action={
+          <>
+            <InputLabel id="period-select" />
+            <Select
+              labelId="period-select"
+              id="demo-simple-select-outlined"
+              value={filter}
+              onChange={e => setFilter(e.target.value)}
+              label="Age"
+            >
+              <MenuItem value="lastMonth">Last Month</MenuItem>
+              <MenuItem value="lastYear">1 Year daily</MenuItem>
+              <MenuItem value="lastYearWeekly">1 Year weekly</MenuItem>
+              <MenuItem value="last5Days">Last 5 days</MenuItem>
+            </Select>
+          </>
+        }
         title="Latest Sales"
       />
       <Divider />
       <CardContent>
-        <Box
-          height={400}
-          position="relative"
-        >
-          <Bar
-            data={data}
-            options={options}
-          />
-        </Box>
+        <div onMouseEnter={disableScroll} onMouseLeave={enableScroll}>
+          {crudeData.length && <CandleChart data={crudeData} />}
+        </div>
       </CardContent>
       <Divider />
-      <Box
-        display="flex"
-        justifyContent="flex-end"
-        p={2}
-      >
+      <Box display="flex" justifyContent="flex-end" p={2}>
         <Button
           color="primary"
           endIcon={<ArrowRightIcon />}
@@ -144,7 +96,7 @@ const Sales = ({ className, ...rest }) => {
 };
 
 Sales.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 export default Sales;
